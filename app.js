5fbc75f5f415eb83f23af509bb16d19a91ba3cbf
Sergey Karlinskiy <s9219871110@gmail.com>
2020-03-30 14:25:48 +0300

    env fixed

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
